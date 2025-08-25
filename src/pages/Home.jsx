6a135183cac4b643afc44386af0576d3456db644
adityaarya03 <adityadrifter@icloud.com>
2025-08-25 18:18:38 +0530

    Fixing-HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ import RatingSlider from '../Components/core/Ratings/RatingSlider';
 
 function Home() {
     const [CatalogPageData, setCatalogPageData] = useState(null);
-    const categoryID = "6475dbeb49dcc886b5698441";
+    const categoryID = "6853e81747f6348dfec1c6df";
 
     useEffect(() => {
         const fetchCatalogPageData = async () => {
